f3aafffb531ad9b704347c7c4e857096bd698e03
SKUMAR-BLRM19 <sumit.kumar@SKUMAR-BLRM19.local>
2019-12-05 11:16:08 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { MyButton } from 'myframeworksumitkumar/dist/MyButton.js'
+
+//give the entire path for accessing the framework from the component
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyButton children={'hello world, lets code!!'} />
     </div>
   );
 }
